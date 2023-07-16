79269dd6fab260ff239db3d9d995da577e5cee56
shahidafridi-swe <shahidafridi.cse@gmail.com>
2023-07-17 03:56:20 +0600

    show all task list

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True,null=True)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Task
+
 
 def tasks(request):
-    return render(request, 'tasks/tasks.html')
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'tasks/tasks.html', context)
