947f40562651adc1fd6cc6919653a958307dd1c3
shahidafridi-swe <shahidafridi.cse@gmail.com>
2023-07-17 16:59:49 +0600

    register user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def registerUser(request):
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('tasks')
+        
+    context={
+        'form': form
+    }
+    return render(request, 'users/register.html', context)
+
+
